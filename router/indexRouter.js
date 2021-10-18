49e7f3fc4b4f8d8aa35f2225099b1f7a44df852b
Lyamir <xlatiax@gmail.com>
2021-10-18 19:05:58 +0800

    Update indexRouter.js

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const router = express()
+const app = express()
+const router = express.Router()
 const controller = require('../controller/indexController')
 
 router.get('/', controller.getIndex)
