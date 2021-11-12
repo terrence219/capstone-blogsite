c02130f0792e23f4b61b721bd2b18188b490a30a
Lyamir <xlatiax@gmail.com>
2021-11-12 23:23:29 +0800

    added seeders

--- model/db.js
+++ model/db.js
@@ -1,16 +1,30 @@
 const mysql = require('mysql')
 const config = require('../config/config.json')
+const fs = require('fs')
 
 const connection = mysql.createConnection({
   host: config.host,
   user: config.user,
-  database: config.database,
-  password: config.password
+  password: config.password,
+  multipleStatements: true
 })
 
 connection.connect(error =>{
   if(error) throw error;
+
+  connection.query(seedQuery, err => {
+    if (err)
+      throw err
+    else
+      console.log("SQL Seed Completed")
+  })
   console.log("DB Connection Successful")
 })
+
+const seedQuery = fs.readFileSync('model/seed.sql', {
+  encoding: 'utf-8',
+})
+
+
 
 module.exports = connection
