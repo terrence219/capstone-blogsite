170eaabfae64867bd315c4deccd650332a90ef08
Lyamir <xlatiax@gmail.com>
2021-12-02 19:28:36 +0800

    updated seeder

--- model/seed.sql
+++ model/seed.sql
@@ -20,17 +20,16 @@
 --
 CREATE DATABASE IF NOT EXISTS capstone_blogsite;
 USE capstone_blogsite;
-DROP TABLE IF EXISTS `posts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `posts` (
+CREATE TABLE IF NOT EXISTS `posts` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` text,
   `content` longtext,
   `date` datetime DEFAULT NULL,
   `author_username` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
