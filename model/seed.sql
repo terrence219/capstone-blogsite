a11e6e7f3ca1d197bddd68f4db9fc0ffaecec9ec
Lyamir <xlatiax@gmail.com>
2021-12-08 22:47:10 +0800

    added comments table

--- model/seed.sql
+++ model/seed.sql
@@ -1,27 +1,6 @@
--- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: capstone_blogsite
--- ------------------------------------------------------
--- Server version	8.0.23
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `posts`
---
 CREATE DATABASE IF NOT EXISTS capstone_blogsite;
 USE capstone_blogsite;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE IF NOT EXISTS `posts` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` text,
@@ -30,25 +9,33 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `author_username` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `posts`
---
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `postID` int NOT NULL,
+  `content` longtext,
+  `date` datetime DEFAULT NULL,
+  `author_username` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`, `postID`),
+  FOREIGN KEY (`postID`) REFERENCES `posts`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-LOCK TABLES `posts` WRITE;
-/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (1,'Goodbye World','This is an updated post','2021-10-27 00:00:00','admin'),(2,'Test Post','This is a test post','2021-10-27 22:26:02','john'),(3,'Test Post 2','This is another test post','2021-10-27 22:28:45','admin');
-/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `posts` (id, title, content, date, author_username) 
+SELECT * FROM (SELECT 1,'Goodbye World','This is an updated post','2021-10-27 00:00:00','admin') AS tmp
+WHERE NOT EXISTS (
+  SELECT id FROM `posts` WHERE id = 1
+) LIMIT 1;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+INSERT INTO `posts` (id, title, content, date, author_username) 
+SELECT * FROM (SELECT 2,'Test Post','This is a test post','2021-10-27 22:26:02','john') AS tmp
+WHERE NOT EXISTS (
+  SELECT id FROM `posts` WHERE id = 2
+) LIMIT 1;
 
--- Dump completed on 2021-11-12 23:07:25
+INSERT INTO `posts` (id, title, content, date, author_username) 
+SELECT * FROM (SELECT 3,'Test Post 2','This is another test post','2021-10-27 22:28:45','admin') AS tmp
+WHERE NOT EXISTS (
+  SELECT id FROM `posts` WHERE id = 3
+) LIMIT 1;
+
+
